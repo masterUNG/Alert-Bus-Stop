20f59344ca80d8f7277297dfc8d8b96c0a60ff5e
masterUNG <phrombutr@gmail.com>
2016-10-31 13:59:32 +0700

    onClick on Add BusStop Finish

--- app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
+++ app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/MainActivity.java
@@ -1,13 +1,51 @@
 package rsu.siriwimon.pakdeeporn.alertbusstop;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.livBusStop);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mySoundEffect(R.raw.add_bus1);
+            }   // onClick
+        });
+
+
+    }   // Main Method
+
+    private void mySoundEffect(int intSound) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), intSound);
+        mediaPlayer.start();
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+            }
+        });
+
+
+    }   // mySoundEffect
+
+}   // Main Class
