36b75f693cc001ea52aaf8a68b3099a0f372c4a8
masterUNG <phrombutr@gmail.com>
2016-11-01 11:59:06 +0700

    Record Sound Finish

--- app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/AddBusStop.java
+++ app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/AddBusStop.java
@@ -1,10 +1,14 @@
 package rsu.siriwimon.pakdeeporn.alertbusstop;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +24,7 @@ public class AddBusStop extends FragmentActivity implements OnMapReadyCallback {
     private EditText editText;
     private Button button;
     private String nameBusStopString;
+    private ImageView recordImageView, playImageView;
 
 
     @Override
@@ -30,6 +35,21 @@ public class AddBusStop extends FragmentActivity implements OnMapReadyCallback {
         //Bind Widget
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button2);
+        recordImageView = (ImageView) findViewById(R.id.imageView4);
+        playImageView = (ImageView) findViewById(R.id.imageView5);
+
+
+        //Record Controller
+        recordImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+                startActivityForResult(intent, 0);
+
+            }   // onClick
+        });
+
 
 
         ///Button Controller
@@ -63,6 +83,19 @@ public class AddBusStop extends FragmentActivity implements OnMapReadyCallback {
     }   // Main Method
 
 
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
+
+            Log.d("1novV1", "Result OK");
+
+        } //if
+
+    }   // onActivityResult
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
